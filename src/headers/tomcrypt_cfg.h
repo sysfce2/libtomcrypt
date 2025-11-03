sysfce2/libtomcrypt

commit fe8e4bf547935da31f24fe1bc76f1444e6ea694d

Use more builtin functions if available
Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/headers/tomcrypt_cfg.h
@@ -295,6 +295,14 @@ typedef unsigned long ltc_mp_digit;
    #define LTC_HAVE_ROTATE_BUILTIN
 #endif
 
+#if __has_builtin(__builtin_clzl)
+   #define LTC_HAVE_CLZL_BUILTIN
+#endif
+
+#if __has_builtin(__builtin_ctzl)
+   #define LTC_HAVE_CTZL_BUILTIN
+#endif
+
 #if defined(__GNUC__)
    #define LTC_ALIGN(n) __attribute__((aligned(n)))
 #else

src/encauth/ocb/ocb_ntz.c
@@ -17,6 +17,11 @@
 */
 int ocb_ntz(unsigned long x)
 {
+#if defined(LTC_HAVE_CTZL_BUILTIN)
+   if (x == 0)
+      return sizeof(unsigned long) * CHAR_BIT;
+   return __builtin_ctzl(x);
+#else
    int c;
    x &= 0xFFFFFFFFUL;
    c = 0;
@@ -25,6 +30,7 @@ int ocb_ntz(unsigned long x)
       x >>= 1;
    }
    return c;
+#endif
 }
 
 #endif

src/encauth/ocb3/ocb3_int_ntz.c
@@ -16,6 +16,11 @@
 */
 int ocb3_int_ntz(unsigned long x)
 {
+#if defined(LTC_HAVE_CTZL_BUILTIN)
+   if (x == 0)
+      return sizeof(unsigned long) * CHAR_BIT;
+   return __builtin_ctzl(x);
+#else
    int c;
    x &= 0xFFFFFFFFUL;
    c = 0;
@@ -24,6 +29,7 @@ int ocb3_int_ntz(unsigned long x)
       x >>= 1;
    }
    return c;
+#endif
 }
 
 #endif

src/mac/pmac/pmac_ntz.c
@@ -14,6 +14,11 @@
 */
 int pmac_ntz(unsigned long x)
 {
+#if defined(LTC_HAVE_CTZL_BUILTIN)
+   if (x == 0)
+      return sizeof(unsigned long) * CHAR_BIT;
+   return __builtin_ctzl(x);
+#else
    int c;
    x &= 0xFFFFFFFFUL;
    c = 0;
@@ -22,6 +27,7 @@ int pmac_ntz(unsigned long x)
       x >>= 1;
    }
    return c;
+#endif
 }
 
 #endif

src/pk/asn1/der/object_identifier/der_length_object_identifier.c
@@ -11,6 +11,11 @@
 
 unsigned long der_object_identifier_bits(unsigned long x)
 {
+#if defined(LTC_HAVE_CLZL_BUILTIN)
+   if (x == 0)
+      return 0;
+   return sizeof(unsigned long) * CHAR_BIT - __builtin_clzl(x);
+#else
    unsigned long c;
    c  = 0;
    while (x) {
@@ -18,8 +23,8 @@ unsigned long der_object_identifier_bits(unsigned long x)
      x >>= 1;
    }
    return c;
+#endif
 }
-
 
 /**
   Gets length of DER encoding of Object Identifier

commit 6f085d349ed7d89ad2540429f9a000df223bc442

Allow disabling of CLZL and CTZL builtins.
Fixes: fe8e4bf5 ("Use more builtin functions if available")
Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/headers/tomcrypt_cfg.h
@@ -246,6 +246,8 @@ typedef unsigned long ltc_mp_digit;
    #undef LTC_FAST
    #define LTC_NO_AES_NI
    #define LTC_NO_BSWAP
+   #define LTC_NO_CLZL
+   #define LTC_NO_CTZL
    #define LTC_NO_ROLC
    #define LTC_NO_ROTATE
 #endif
@@ -296,11 +298,11 @@ typedef unsigned long ltc_mp_digit;
    #define LTC_HAVE_ROTATE_BUILTIN
 #endif
 
-#if __has_builtin(__builtin_clzl)
+#if !defined(LTC_NO_CLZL) && __has_builtin(__builtin_clzl)
    #define LTC_HAVE_CLZL_BUILTIN
 #endif
 
-#if __has_builtin(__builtin_ctzl)
+#if !defined(LTC_NO_CTZL) && __has_builtin(__builtin_ctzl)
    #define LTC_HAVE_CTZL_BUILTIN
 #endif
 
sysfce2/libtomcrypt

commit 6daff83d8239ace38aaad6062a450c277ebe2b7c

Deprecate `compare_testvector()`.
This should never have been public API.
Also introduce a `LTC_COMPARE_TESTVECTOR()` macro to be used within the
library, which doesn't exit, but only returns an error.

Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/headers/tomcrypt_private.h
@@ -393,6 +393,17 @@ struct get_char {
 void copy_or_zeromem(const unsigned char* src, unsigned char* dest, unsigned long len, int coz);
 void password_free(struct password *pw, const struct password_ctx *ctx);
 
+int ltc_compare_testvector(const void* is, const unsigned long is_len, const void* should, const unsigned long should_len, const char* what, int which);
+int ltc_do_compare_testvector(const void* is, const unsigned long is_len, const void* should, const unsigned long should_len, const char* what, int which);
+
+#define LTC_COMPARE_TESTVECTOR(i, il, s, sl, wa, wi)                                   \
+   do {                                                                                \
+      int LTC_TMPVAR(ret) = do_compare_testvector((i), (il), (s), (sl), (wa), (wi));   \
+      if (LTC_TMPVAR(ret) != CRYPT_OK) {                                               \
+         return LTC_TMPVAR(ret);                                                       \
+      }                                                                                \
+   } while(0)
+
 #if defined(LTC_PBES)
 int pbes_decrypt(const pbes_arg  *arg, unsigned char *dec_data, unsigned long *dec_size);
 

commit e33128e18c806c0d458d83345b7b1fe379d39758

Fix `LTC_COMPARE_TESTVECTOR()`
Fixes: 6daff83d ("Deprecate `compare_testvector()`.")
Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/headers/tomcrypt_private.h
@@ -396,12 +396,12 @@ void password_free(struct password *pw, const struct password_ctx *ctx);
 int ltc_compare_testvector(const void* is, const unsigned long is_len, const void* should, const unsigned long should_len, const char* what, int which);
 int ltc_do_compare_testvector(const void* is, const unsigned long is_len, const void* should, const unsigned long should_len, const char* what, int which);
 
-#define LTC_COMPARE_TESTVECTOR(i, il, s, sl, wa, wi)                                   \
-   do {                                                                                \
-      int LTC_TMPVAR(ret) = do_compare_testvector((i), (il), (s), (sl), (wa), (wi));   \
-      if (LTC_TMPVAR(ret) != CRYPT_OK) {                                               \
-         return LTC_TMPVAR(ret);                                                       \
-      }                                                                                \
+#define LTC_COMPARE_TESTVECTOR(i, il, s, sl, wa, wi)                                      \
+   do {                                                                                   \
+      int LTC_TMPVAR(ret) = ltc_do_compare_testvector((i), (il), (s), (sl), (wa), (wi));  \
+      if (LTC_TMPVAR(ret) != CRYPT_OK) {                                                  \
+         return LTC_TMPVAR(ret);                                                          \
+      }                                                                                   \
    } while(0)
 
 #if defined(LTC_PBES)
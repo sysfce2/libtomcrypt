sysfce2/libtomcrypt

commit 076a11037aa68cd78d9ed0e476a09e0995e10f13

Ed448 + X448

src/pk/asn1/x509/x509_import_spki.c
@@ -52,6 +52,10 @@ static const import_fn s_import_spki_fns[LTC_PKA_NUM] = {
                                                 [LTC_PKA_X25519] = (import_fn)s_x25519_import_spki,
                                                 [LTC_PKA_ED25519] = (import_fn)s_ed25519_import_spki,
 #endif
+#ifdef LTC_CURVE448
+                                                [LTC_PKA_X448] = (import_fn)x448_import_x509,
+                                                [LTC_PKA_ED448] = (import_fn)ed448_import_x509,
+#endif
 };
 
 int x509_import_spki(const unsigned char *asn1_cert, unsigned long asn1_len, ltc_pka_key *k, ltc_asn1_list **root)

doc/crypt.tex
@@ -5933,15 +5933,20 @@ ECCEncrypt ::= SEQUENCE {
 \mysection{Introduction}
 
 The library provides functionality for \textit{Curve25519}-based \textit{X25519} Diffie-Hellman shared secrets
-and \textit{EdDSA} a.k.a \textit{Ed25519} signature schemes.
+and \textit{EdDSA} a.k.a \textit{Ed25519} signature schemes as well as the corresponding
+\textit{Curve448}/\textit{ec448}-based \textit{X448} and \textit{Ed448} algorithms.
 
-The implementation is based on the \textit{tweetnacl}\footnote{\url{https://tweetnacl.cr.yp.to/}} reference implementation
+The Curve25519 implementation is based on the \textit{tweetnacl}\footnote{\url{https://tweetnacl.cr.yp.to/}} reference implementation
 as provided by Daniel J. Bernstein et.al. and only slightly modified to better fit in the library.
+The Curve448/\textit{ec448} implementation follows the same overall code structure and arithmetic layout pattern.
 
-Both algorithms share the key structure called \textit{curve25519\_key} which is used by all Curve25519 functions.
+The Curve25519 family shares the key structure called \textit{curve25519\_key} which is used by all
+Curve25519 functions. The Curve448 family uses the \textit{curve448\_key} structure.
 
 As Curve25519 and Ed25519 are based on the same elliptic curve, but use different mathematics, the keys are not
 compatible to each other.
+
+Likewise X448 and Ed448 keys are not compatible to each other.
 
 It is possible to convert a Curve-Key to an Ed-Key and vice-versa, but this is not provided (yet).
 
@@ -5961,7 +5966,7 @@ int x25519_make_key(    prng_state *prng,
                     curve25519_key *key);
 \end{verbatim}
 
-To generate a fresh X25529 key, one can use \textit{x25519\_make\_key} which will create a private\&public key-pair.
+To generate a fresh X25519 key, one can use \textit{x25519\_make\_key} which will create a private\&public key-pair.
 \index{x25519\_import}
 \begin{verbatim}
 int x25519_import(const  unsigned char *in,
@@ -6056,7 +6061,7 @@ int ed25519_make_key(    prng_state *prng,
                      curve25519_key *key);
 \end{verbatim}
 
-To generate a fresh Ed25529 key, one can use \textit{ed25519\_make\_key} which will create a private\&public key-pair.
+To generate a fresh Ed25519 key, one can use \textit{ed25519\_make\_key} which will create a private\&public key-pair.
 
 \index{ed25519\_import}
 \begin{verbatim}
@@ -6166,6 +6171,232 @@ int ed25519ph_verify(const  unsigned char *msg, unsigned long msglen,
                      const  unsigned char *ctx, unsigned long ctxlen,
                                       int *stat,
                      const curve25519_key *public_key);
+\end{verbatim}
+
+These functions will verify the EdDSA signature in the array pointed to by \textit{sig} of length \textit{siglen} octets, against the message
+pointed to by the array \textit{msg} of length \textit{msglen}. It will store a non--zero value in \textit{stat} if the signature is valid.  Note:
+the function will not return an error if the signature is invalid. It will only return an error if the actual signature payload is an invalid format.
+
+\mysection{Curve448-based Diffie-Hellman Key Exchange - X448}
+
+The library provides the Diffie-Hellman Key Exchange algorithm \textit{X448} for curve448 as specified in RFC 7748.
+
+\subsection{X448 Key Operations}
+
+The \textit{X448} algorithm API provides the following set of functions to create, import and export keys.
+
+\index{x448\_make\_key}
+\begin{verbatim}
+int x448_make_key(    prng_state *prng,
+                             int  wprng,
+                    curve448_key *key);
+\end{verbatim}
+
+To generate a fresh X448 key, one can use \textit{x448\_make\_key} which will create a private\&public key-pair.
+\index{x448\_import}
+\begin{verbatim}
+int x448_import(const  unsigned char *in,
+                       unsigned long  inlen,
+                      curve448_key *key);
+\end{verbatim}
+
+The \textit{x448\_import} function can be used to import a public key in DER-encoded \textit{SubjectPublicKeyInfo} format.
+
+\index{x448\_import\_raw}
+\begin{verbatim}
+int x448_import_raw(const  unsigned char *in,
+                           unsigned long  inlen,
+                                     int  which,
+                          curve448_key *key);
+\end{verbatim}
+
+To import a public or private key in raw format, one can use the function \textit{x448\_import\_raw}.
+
+\index{x448\_import\_x509}
+\begin{verbatim}
+int x448_import_x509(const  unsigned char *in,
+                            unsigned long  inlen,
+                           curve448_key *key);
+\end{verbatim}
+
+To import a public key from a DER-encoded \textit{X.509} certificate, one can use the function \textit{x448\_import\_x509}.
+
+\index{x448\_import\_pkcs8}
+\begin{verbatim}
+int x448_import_pkcs8(const  unsigned char *in,
+                             unsigned long  inlen,
+                      const   password_ctx *pw_ctx,
+                            curve448_key *key);
+\end{verbatim}
+
+To import a private key in the \textit{OneAsymmetricKey} a.k.a \textit{PKCS \#8} format, either plain or PBES encrypted,
+one can use the function \textit{x448\_import\_pkcs8}.
+
+\index{x448\_export}
+\begin{verbatim}
+int x448_export(       unsigned char *out,
+                       unsigned long *outlen,
+                                 int  which,
+                const curve448_key *key);
+\end{verbatim}
+
+To export a key, the function \textit{x448\_export} is provided.
+
+It has support for the following output formats:
+
+\begin{figure}[H]
+\begin{center}
+\begin{tabular}{|c|c|}
+\hline \textbf{which} & \textbf{output format} \\
+\hline PK\_PRIVATE & Raw \\
+\hline PK\_PRIVATE \& PK\_STD & PKCS \#8 \\
+\hline PK\_PUBLIC & Raw \\
+\hline PK\_PUBLIC \& PK\_STD & SubjectPublicKeyInfo \\
+\hline
+\end{tabular}
+\end{center}
+\caption{Possible x448\_export() output formats}
+\end{figure}
+
+\subsection{X448 Cryptographic Operations}
+
+To construct a Diffie-Hellman shared secret with a private and a public X448 key, use the following function:
+
+\index{x448\_shared\_secret}
+\begin{verbatim}
+int x448_shared_secret(const curve448_key *private_key,
+                       const curve448_key *public_key,
+                             unsigned char *out,
+                             unsigned long *outlen);
+\end{verbatim}
+
+This will construct the shared secret between the private- and the public-key and store the result in \textit{out} of length \textit{outlen}.
+
+\mysection{Curve448-based EdDSA Signature Scheme - Ed448}
+
+The library provides the EdDSA algorithm for the edwards448 curve in the PureEdDSA variant as specified in RFC 8032.
+
+\subsection{Ed448 Key Operations}
+
+The \textit{Ed448} algorithm API provides the following set of functions to create, import and export keys.
+
+\index{ed448\_make\_key}
+\begin{verbatim}
+int ed448_make_key(    prng_state *prng,
+                              int  wprng,
+                    curve448_key *key);
+\end{verbatim}
+
+To generate a fresh Ed448 key, one can use \textit{ed448\_make\_key} which will create a private\&public key-pair.
+
+\index{ed448\_import}
+\begin{verbatim}
+int ed448_import(const  unsigned char *in,
+                        unsigned long  inlen,
+                       curve448_key *key);
+\end{verbatim}
+
+The \textit{ed448\_import} function can be used to import a public key in DER-encoded \textit{SubjectPublicKeyInfo} format.
+
+\index{ed448\_import\_raw}
+\begin{verbatim}
+int ed448_import_raw(const  unsigned char *in,
+                            unsigned long  inlen,
+                                      int  which,
+                           curve448_key *key);
+\end{verbatim}
+
+To import a public or private key in raw format, one can use the function \textit{ed448\_import\_raw}.
+
+\index{ed448\_import\_x509}
+\begin{verbatim}
+int ed448_import_x509(const  unsigned char *in,
+                             unsigned long inlen,
+                            curve448_key *key);
+\end{verbatim}
+
+To import a public key from a DER-encoded \textit{X.509} certificate, one can use the function \textit{ed448\_import\_x509}.
+
+\index{ed448\_import\_pkcs8}
+\begin{verbatim}
+int ed448_import_pkcs8(const  unsigned char *in,
+                              unsigned long  inlen,
+                       const   password_ctx *pw_ctx,
+                             curve448_key *key);
+\end{verbatim}
+
+To import a private key in the \textit{OneAsymmetricKey} a.k.a \textit{PKCS \#8} format, either plain or PBES encrypted,
+one can use the function \textit{ed448\_import\_pkcs8}.
+
+\index{ed448\_export}
+\begin{verbatim}
+int ed448_export(       unsigned char *out,
+                        unsigned long *outlen,
+                                  int  which,
+                 const curve448_key *key);
+\end{verbatim}
+
+To export a key, the function \textit{ed448\_export} is provided.
+
+It has support for the following output formats:
+
+\begin{figure}[H]
+\begin{center}
+\begin{tabular}{|c|c|}
+\hline \textbf{which} & \textbf{output format} \\
+\hline PK\_PRIVATE & Raw \\
+\hline PK\_PRIVATE \& PK\_STD & PKCS \#8 \\
+\hline PK\_PUBLIC & Raw \\
+\hline PK\_PUBLIC \& PK\_STD & SubjectPublicKeyInfo \\
+\hline
+\end{tabular}
+\end{center}
+\caption{Possible ed448\_export() output formats}
+\end{figure}
+
+\subsection{Ed448 Cryptographic Operations}
+
+To sign and/or verify a message use the following functions:
+
+\index{ed448\_sign}
+\index{ed448ctx\_sign}
+\index{ed448ph\_sign}
+\begin{verbatim}
+int ed448_sign(const  unsigned char *msg, unsigned long msglen,
+                      unsigned char *sig, unsigned long *siglen,
+               const curve448_key *private_key);
+int ed448ctx_sign(const  unsigned char *msg, unsigned long  msglen,
+                         unsigned char *sig, unsigned long *siglen,
+                  const  unsigned char *ctx, unsigned long  ctxlen,
+                  const curve448_key *private_key);
+int ed448ph_sign(const  unsigned char *msg, unsigned long  msglen,
+                        unsigned char *sig, unsigned long *siglen,
+                 const  unsigned char *ctx, unsigned long  ctxlen,
+                 const curve448_key *private_key);
+\end{verbatim}
+
+These functions will EdDSA sign the message stored in the array pointed to by \textit{msg} of length \textit{msglen} octets.  The signature
+will be stored in the array pointed to by \textit{sig} of length \textit{siglen} octets.  The \texttt{ctx} and \texttt{ph} variants also
+allow passing a context \textit{ctx} of length \textit{ctxlen} octets.  This context is allowed to be max. 255 octets long.
+
+\index{ed448\_verify}
+\index{ed448ctx\_verify}
+\index{ed448ph\_verify}
+\begin{verbatim}
+int ed448_verify(const  unsigned char *msg, unsigned long msglen,
+                 const  unsigned char *sig, unsigned long siglen,
+                                  int *stat,
+                 const curve448_key *public_key);
+int ed448ctx_verify(const  unsigned char *msg, unsigned long msglen,
+                    const  unsigned char *sig, unsigned long siglen,
+                    const  unsigned char *ctx, unsigned long ctxlen,
+                                     int *stat,
+                    const curve448_key *public_key);
+int ed448ph_verify(const  unsigned char *msg, unsigned long msglen,
+                   const  unsigned char *sig, unsigned long siglen,
+                   const  unsigned char *ctx, unsigned long ctxlen,
+                                    int *stat,
+                   const curve448_key *public_key);
 \end{verbatim}
 
 These functions will verify the EdDSA signature in the array pointed to by \textit{sig} of length \textit{siglen} octets, against the message
@@ -6477,6 +6708,8 @@ enum ltc_pka_id {
    LTC_PKA_X25519,
    LTC_PKA_ED25519,
    LTC_PKA_DH,
+   LTC_PKA_X448,
+   LTC_PKA_ED448,
 };
 
 typedef struct {
@@ -6484,6 +6717,10 @@ typedef struct {
 #ifdef LTC_CURVE25519
       curve25519_key x25519;
       curve25519_key ed25519;
+#endif
+#ifdef LTC_CURVE448
+      curve448_key x448;
+      curve448_key ed448;
 #endif
 #ifdef LTC_MDH
       dh_key dh;
@@ -7766,14 +8003,14 @@ The identifiers in the PEM headers recognized are as follows:
 \begin{table}[H]
 \begin{minipage}{\textwidth}
 \begin{small}
-\begin{tabular}{|l|l|l|l|l|}
+\begin{tabular}{|l|l|l|l|p{4.1cm}|}
 \hline \textbf{Identifier}                   & \textbf{Key type} & \textbf{File content} & \textbf{Standard} & \textbf{Algorithm} \\
-\hline \texttt{BEGIN CERTIFICATE}            & Public            & Plain                 & \texttt{X.509}    & DH, DSA, ECC, Ed25519, RSA, X25519 \\
+\hline \texttt{BEGIN CERTIFICATE}            & Public            & Plain                 & \texttt{X.509}    & DH, DSA, ECC, Ed25519, Ed448, RSA, X25519, X448 \\
 \hline \texttt{BEGIN DSA PRIVATE KEY}        & Private           & Maybe encrypted       & \texttt{OpenSSL\footnote{There are two de-facto standard for DSA private key structures, LibTomCrypt implements OpenSSL's}} & DSA \\
 \hline \texttt{BEGIN EC PRIVATE KEY}         & Private           & Maybe encrypted       & \texttt{RFC 5915} & ECC \\
-\hline \texttt{BEGIN ENCRYPTED PRIVATE KEY}  & Private           & Encrypted             & \texttt{PKCS \#8} & DH, DSA, ECC, Ed25519, RSA, X25519 \\
-\hline \texttt{BEGIN PRIVATE KEY}            & Private           & Plain                 & \texttt{PKCS \#8} & DH, DSA, ECC, Ed25519, RSA, X25519 \\
-\hline \texttt{BEGIN PUBLIC KEY}             & Public            & Plain                 & \texttt{X.509\footnote{Specifically, SubjectPublicKeyInfo}} & DH, DSA, ECC, Ed25519, RSA, X25519 \\
+\hline \texttt{BEGIN ENCRYPTED PRIVATE KEY}  & Private           & Encrypted             & \texttt{PKCS \#8} & DH, DSA, ECC, Ed25519, Ed448, RSA, X25519, X448 \\
+\hline \texttt{BEGIN PRIVATE KEY}            & Private           & Plain                 & \texttt{PKCS \#8} & DH, DSA, ECC, Ed25519, Ed448, RSA, X25519, X448 \\
+\hline \texttt{BEGIN PUBLIC KEY}             & Public            & Plain                 & \texttt{X.509\footnote{Specifically, SubjectPublicKeyInfo}} & DH, DSA, ECC, Ed25519, Ed448, RSA, X25519, X448 \\
 \hline \texttt{BEGIN RSA PRIVATE KEY}        & Private           & Maybe encrypted       & \texttt{PKCS \#1} & RSA \\
 \hline \texttt{BEGIN RSA PUBLIC KEY}         & Public            & Plain                 & \texttt{PKCS \#1} & RSA \\
 \hline
@@ -8240,8 +8477,11 @@ static const char *pka_algos[] = {
    "RSA",
    "DSA",
    "ECC",
-   "Curve25519",
+   "X25519",
+   "Ed25519",
    "DH",
+   "X448",
+   "Ed448",
 };
 
 static int password_get(void **p, unsigned long *l, void *u)

src/headers/tomcrypt_custom.h
@@ -454,6 +454,9 @@
 /* Ed25519 & X25519 */
 #define LTC_CURVE25519
 
+/* Ed448 & X448 */
+#define LTC_CURVE448
+
 /* ECC */
 #define LTC_MECC
 

src/headers/tomcrypt_pk.h
@@ -41,6 +41,8 @@ enum ltc_pka_id {
    LTC_PKA_ED25519,
    LTC_PKA_DH,
    LTC_PKA_RSA_PSS,
+   LTC_PKA_X448,
+   LTC_PKA_ED448,
    LTC_PKA_NUM
 };
 
@@ -629,6 +631,80 @@ int x25519_shared_secret(const curve25519_key *private_key,
 
 #endif /* LTC_CURVE25519 */
 
+
+#ifdef LTC_CURVE448
+
+typedef struct {
+    /** Type of key, PK_PRIVATE or PK_PUBLIC */
+    enum public_key_type type;
+    /** Algorithm: LTC_PKA_X448 or LTC_PKA_ED448 */
+    enum ltc_pka_id pka;
+    /** The private key (56 bytes for X448, 57 bytes for Ed448) */
+    unsigned char priv[57];
+    /** The public key (56 bytes for X448, 57 bytes for Ed448) */
+    unsigned char pub[57];
+} curve448_key;
+
+/** X448 Diffie-Hellman key exchange */
+int x448_make_key(prng_state *prng, int wprng, curve448_key *key);
+
+int x448_export(       unsigned char *out, unsigned long *outlen,
+                                 int  which,
+                const curve448_key *key);
+
+int x448_import(const unsigned char *in, unsigned long inlen, curve448_key *key);
+int x448_import_raw(const unsigned char *in, unsigned long inlen, int which, curve448_key *key);
+int x448_import_x509(const unsigned char *in, unsigned long inlen, curve448_key *key);
+int x448_import_pkcs8(const unsigned char  *in, unsigned long inlen,
+                      const password_ctx   *pw_ctx,
+                            curve448_key *key);
+
+int x448_shared_secret(const curve448_key *private_key,
+                       const curve448_key *public_key,
+                             unsigned char *out, unsigned long *outlen);
+
+/** Ed448 Signature API */
+int ed448_make_key(prng_state *prng, int wprng, curve448_key *key);
+
+int ed448_export(       unsigned char *out, unsigned long *outlen,
+                                  int  which,
+                 const curve448_key *key);
+
+int ed448_import(const unsigned char *in, unsigned long inlen, curve448_key *key);
+int ed448_import_raw(const unsigned char *in, unsigned long inlen, int which, curve448_key *key);
+int ed448_import_x509(const unsigned char *in, unsigned long inlen, curve448_key *key);
+int ed448_import_pkcs8(const unsigned char  *in, unsigned long inlen,
+                       const password_ctx   *pw_ctx,
+                             curve448_key *key);
+
+int ed448_sign(const  unsigned char *msg, unsigned long msglen,
+                      unsigned char *sig, unsigned long *siglen,
+               const curve448_key *private_key);
+int ed448ctx_sign(const  unsigned char *msg, unsigned long  msglen,
+                         unsigned char *sig, unsigned long *siglen,
+                  const  unsigned char *ctx, unsigned long  ctxlen,
+                  const curve448_key *private_key);
+int ed448ph_sign(const  unsigned char *msg, unsigned long  msglen,
+                        unsigned char *sig, unsigned long *siglen,
+                 const  unsigned char *ctx, unsigned long  ctxlen,
+                 const curve448_key *private_key);
+int ed448_verify(const  unsigned char *msg, unsigned long msglen,
+                 const  unsigned char *sig, unsigned long siglen,
+                                  int *stat,
+                 const curve448_key *public_key);
+int ed448ctx_verify(const  unsigned char *msg, unsigned long msglen,
+                    const  unsigned char *sig, unsigned long siglen,
+                    const  unsigned char *ctx, unsigned long ctxlen,
+                                     int *stat,
+                    const curve448_key *public_key);
+int ed448ph_verify(const  unsigned char *msg, unsigned long msglen,
+                   const  unsigned char *sig, unsigned long siglen,
+                   const  unsigned char *ctx, unsigned long ctxlen,
+                                    int *stat,
+                   const curve448_key *public_key);
+
+#endif /* LTC_CURVE448 */
+
 #ifdef LTC_MDSA
 
 /* Max diff between group and modulus size in bytes (max case: L=8192bits, N=256bits) */
@@ -723,6 +799,10 @@ typedef struct {
 #ifdef LTC_CURVE25519
       curve25519_key x25519;
       curve25519_key ed25519;
+#endif
+#ifdef LTC_CURVE448
+      curve448_key x448;
+      curve448_key ed448;
 #endif
 #ifdef LTC_MDH
       dh_key dh;

src/headers/tomcrypt_private.h
@@ -59,6 +59,8 @@ enum ltc_oid_id {
    LTC_OID_EC_PRIMEF,
    LTC_OID_X25519,
    LTC_OID_ED25519,
+   LTC_OID_X448,
+   LTC_OID_ED448,
    LTC_OID_DH,
    LTC_OID_RSA_OAEP,
    LTC_OID_RSA_MGF1,
@@ -712,6 +714,33 @@ int ec25519_crypto_ctx(      unsigned char *out, unsigned long *outlen,
                              unsigned char flag,
                        const unsigned char *ctx, unsigned long  ctxlen);
 #endif /* LTC_CURVE25519 */
+
+#ifdef LTC_CURVE448
+int ec448_export(unsigned char *out, unsigned long *outlen, int which, const curve448_key *key);
+int ec448_import_pkcs8(const unsigned char *in, unsigned long inlen,
+                       const password_ctx *pw_ctx, enum ltc_oid_id id, curve448_key *key);
+int ec448_import_pkcs8_asn1(ltc_asn1_list *alg_id, ltc_asn1_list *priv_key,
+                            enum ltc_oid_id id, curve448_key *key);
+int x448_import_pkcs8_asn1(ltc_asn1_list *alg_id, ltc_asn1_list *priv_key,
+                           curve448_key *key);
+int ec448_keypair_internal(prng_state *prng, int wprng, unsigned char *pk, unsigned char *sk);
+int ec448_sk_to_pk_internal(unsigned char *pk, const unsigned char *sk);
+int ec448_sign_internal(unsigned char *sm, unsigned long long *smlen,
+                        const unsigned char *m, unsigned long long mlen,
+                        const unsigned char *sk, const unsigned char *pk,
+                        const unsigned char *ctx, unsigned long long cs);
+int ec448_verify_internal(int *stat, unsigned char *m, unsigned long long *mlen,
+                          const unsigned char *sm, unsigned long long smlen,
+                          const unsigned char *ctx, unsigned long long cs,
+                          const unsigned char *pk);
+int ec448_prehash_internal(unsigned char *out, const unsigned char *msg, unsigned long long msglen);
+int ec448_crypto_ctx(unsigned char *out, unsigned long *outlen, unsigned char flag,
+                   const unsigned char *ctx, unsigned long ctxlen);
+int ec448_scalarmult_internal(unsigned char *out, const unsigned char *scalar, const unsigned char *point);
+int ec448_scalarmult_base_internal(unsigned char *out, const unsigned char *scalar);
+int ed448_import_pkcs8_asn1(ltc_asn1_list *alg_id, ltc_asn1_list *priv_key,
+                            curve448_key *key);
+#endif /* LTC_CURVE448 */
 
 #ifdef LTC_DER
 

src/misc/crypt/crypt.c
@@ -384,6 +384,10 @@ const char *crypt_build_settings =
     "   X25519\n"
 #endif
 #endif
+#if defined(LTC_CURVE448)
+    "   Ed448\n"
+    "   X448\n"
+#endif
 #if defined(LTC_PK_MAX_RETRIES)
     "   "NAME_VALUE(LTC_PK_MAX_RETRIES)"\n"
 #endif

src/misc/pem/pem_pkcs.c
@@ -63,6 +63,10 @@ static const struct {
                                                 [LTC_OID_X25519] =  { LTC_PKA_X25519, (pkcs8_import_fn)x25519_import_pkcs8_asn1 },
                                                 [LTC_OID_ED25519] = { LTC_PKA_ED25519, (pkcs8_import_fn)ed25519_import_pkcs8_asn1 },
 #endif
+#ifdef LTC_CURVE448
+                                                [LTC_OID_X448] =  { LTC_PKA_X448, (pkcs8_import_fn)x448_import_pkcs8_asn1 },
+                                                [LTC_OID_ED448] = { LTC_PKA_ED448, (pkcs8_import_fn)ed448_import_pkcs8_asn1 },
+#endif
 };
 
 static int s_import_pkcs8(unsigned char *asn1_cert, unsigned long asn1_len, ltc_pka_key *k, const password_ctx *pw_ctx)
@@ -121,6 +125,10 @@ static const import_fn s_import_openssl_fns[LTC_PKA_NUM] = {
 #ifdef LTC_CURVE25519
                                                 [LTC_PKA_X25519] = (import_fn)x25519_import,
                                                 [LTC_PKA_ED25519] = (import_fn)ed25519_import,
+#endif
+#ifdef LTC_CURVE448
+                                                [LTC_PKA_X448] = (import_fn)x448_import,
+                                                [LTC_PKA_ED448] = (import_fn)ed448_import,
 #endif
 };
 

src/pk/asn1/oid/pk_get.c
@@ -19,6 +19,8 @@ static const oid_table_entry pka_oids[] = {
                                               { LTC_OID_EC_PRIMEF,            LTC_PKA_EC,      NULL,         "1.2.840.10045.1.1" },
                                               { LTC_OID_X25519,               LTC_PKA_X25519,  NULL,         "1.3.101.110" },
                                               { LTC_OID_ED25519,              LTC_PKA_ED25519, NULL,         "1.3.101.112" },
+                                              { LTC_OID_X448,                 LTC_PKA_X448,    NULL,         "1.3.101.111" },
+                                              { LTC_OID_ED448,                LTC_PKA_ED448,   NULL,         "1.3.101.113" },
                                               { LTC_OID_DH,                   LTC_PKA_DH,      NULL,         "1.2.840.113549.1.3.1" },
                                               { LTC_OID_RSA_OAEP,             LTC_PKA_RSA,     NULL,         "1.2.840.113549.1.1.7" },
                                               { LTC_OID_RSA_MGF1,             LTC_PKA_RSA,     NULL,         "1.2.840.113549.1.1.8" },

commit 5a2bc6b1314878b2a8ec7777e96395b422fb13ec

Fix importing certificate with Ed448 key.
While running the tests in verbose mode I saw that
`tests/pem/openssl_ed448_x509.pem` is skipped instead of processed.

Fixes: 076a11037aa6 ("Ed448 + X448")
Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/pk/asn1/x509/x509_import_spki.c
@@ -37,6 +37,32 @@ static int s_ed25519_import_spki(const unsigned char *in, unsigned long inlen, v
 }
 #endif
 
+#ifdef LTC_CURVE448
+static int s_x448_import_pub(const unsigned char *in, unsigned long inlen, void *key)
+{
+   return x448_import_raw(in, inlen, PK_PUBLIC, key);
+}
+static int s_x448_import_spki(const unsigned char *in, unsigned long inlen, void *key)
+{
+   return x509_process_public_key_from_spki(in, inlen,
+                                            LTC_OID_X448,
+                                            LTC_ASN1_EOL, NULL, NULL,
+                                            s_x448_import_pub, key);
+}
+
+static int s_ed448_import_pub(const unsigned char *in, unsigned long inlen, void *key)
+{
+   return ed448_import_raw(in, inlen, PK_PUBLIC, key);
+}
+static int s_ed448_import_spki(const unsigned char *in, unsigned long inlen, void *key)
+{
+   return x509_process_public_key_from_spki(in, inlen,
+                                            LTC_OID_ED448,
+                                            LTC_ASN1_EOL, NULL, NULL,
+                                            s_ed448_import_pub, key);
+}
+#endif
+
 static const import_fn s_import_spki_fns[LTC_PKA_NUM] = {
 #ifdef LTC_MRSA
                                                 [LTC_PKA_RSA] = (import_fn)rsa_import_spki,
@@ -53,8 +79,8 @@ static const import_fn s_import_spki_fns[LTC_PKA_NUM] = {
                                                 [LTC_PKA_ED25519] = (import_fn)s_ed25519_import_spki,
 #endif
 #ifdef LTC_CURVE448
-                                                [LTC_PKA_X448] = (import_fn)x448_import_x509,
-                                                [LTC_PKA_ED448] = (import_fn)ed448_import_x509,
+                                                [LTC_PKA_X448] = (import_fn)s_x448_import_spki,
+                                                [LTC_PKA_ED448] = (import_fn)s_ed448_import_spki,
 #endif
 };
 